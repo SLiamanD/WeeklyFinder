9e521ee20d0cfaa528f4e493391b10863c6f7755
SLiamanD <sliamand@mail.ru>
2021-09-20 16:32:16 +0300

    test commit

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dataTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
     }
-
+    
 
 }
 
