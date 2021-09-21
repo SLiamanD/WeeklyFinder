0827751c55d5165d31c2614de29ccb981fa6bebe
SLiamanD <sliamand@mail.ru>
2021-09-21 21:36:33 +0300

    first version is ready

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -23,6 +23,15 @@ class ViewController: UIViewController {
         dateComponents.day = Int(dateTF.text!)
         dateComponents.month = Int(monthTF.text!)
         dateComponents.year = Int(yearTF.text!)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        let date = calendar.date(from: dateComponents)
+        let weekday = dateFormatter.string(from: date!)
+        
+        resultLabel.text = weekday`
+        
     }
     
 }
