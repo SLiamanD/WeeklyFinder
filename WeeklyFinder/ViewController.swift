90ae8b3f8f0fb46b5b8776a40743a248173322d4
SLiamanD <sliamand@mail.ru>
2021-09-29 21:37:39 +0300

    forced unwrap

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -14,26 +14,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var monthTF: UITextField!
     @IBOutlet weak var yearTF: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
+    
+    
     @IBAction func findDay(_ sender: UIButton) {
         
-        let calendar = Calendar.current
-        var dateComponents = DateComponents()
-        dateComponents.day = Int(dateTF.text!)
-        dateComponents.month = Int(monthTF.text!)
-        dateComponents.year = Int(yearTF.text!)
+        if let day = dateTF.text, let month = monthTF.text, let year = yearTF.text {
+            
+            
+            let calendar = Calendar.current
+            var dateComponents = DateComponents()
+            dateComponents.day = Int(dateTF.text!)
+            dateComponents.month = Int(monthTF.text!)
+            dateComponents.year = Int(yearTF.text!)
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "EEEE"
+            
+            let date = calendar.date(from: dateComponents)
+            let weekday = dateFormatter.string(from: date!)
+            
+            resultLabel.text = weekday
+            
+        }
+        func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.view.endEditing(true)
+        }
+            
+        }
         
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEEE"
         
-        let date = calendar.date(from: dateComponents)
-        let weekday = dateFormatter.string(from: date!)
-        
-        resultLabel.text = weekday
-        
-    }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
+
     
     
     
